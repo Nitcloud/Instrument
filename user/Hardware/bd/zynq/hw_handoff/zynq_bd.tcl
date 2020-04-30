7efee8497f83cafa8ce82d9253b0369253c36781
sterben <1579729633@qq.com>
2020-04-30 15:56:59 +0800

    2020/4/30

--- user/Hardware/bd/zynq/hw_handoff/zynq_bd.tcl
+++ user/Hardware/bd/zynq/hw_handoff/zynq_bd.tcl
@@ -65,7 +65,7 @@ set run_remote_bd_flow 1
 if { $run_remote_bd_flow == 1 } {
   # Set the reference directory for source file relative paths (by default 
   # the value is script directory path)
-  set origin_dir ./user/Hardware/bd
+  set origin_dir ./bd
 
   # Use origin directory path location variable, if specified in the tcl shell
   if { [info exists ::origin_dir_loc] } {
